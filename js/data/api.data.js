c697fd8710ce1665bfeced7a4046224591e3d4d5
iarbaizaBirtlh <119678479+iarbaizaBirtlh@users.noreply.github.com>
2025-11-22 22:36:40 +0100

    change deezer

--- js/data/api.data.js
+++ js/data/api.data.js
@@ -1,4 +1,4 @@
-const ITUNES_URL = "https://itunes.apple.com/search";
+/* const ITUNES_URL = "https://itunes.apple.com/search";
 
 export async function getTopTracks(term = "pop",limit = 20) {
     const response = await fetch(`${ITUNES_URL}?term=${encodeURIComponent(term)}&entity=song&limit=${limit}`);
@@ -10,5 +10,21 @@ export async function getTrackById(id) {
     const response = await fetch(`https://itunes.apple.com/lookup?id=${id}`);
     const data = await response.json();
     return data.results?.[0] || null;
+} */
+
+const DEEZER_SEARCH = "https://api.deezer.com/search";
+const DEEZER_TRACK = "https://api.deezer.com/track";
+
+export async function getTopTracks(artist = "eminem", limit = 20) {
+    const response = await fetch(`https://api.allorigins.win/get?url=${encodeURIComponent(`${DEEZER_SEARCH}?q=artist:"${artist}"&limit=${limit}`)}`);
+    const data = await response.json();
+    const results = JSON.parse(data.contents).data; // Deezer wrapped in proxy
+    return results;
+}
+
+export async function getTrackById(id) {
+    const response = await fetch(`https://api.allorigins.win/get?url=${encodeURIComponent(`${DEEZER_TRACK}/${id}`)}`);
+    const data = await response.json();
+    return JSON.parse(data.contents); // Track object
 }
 
